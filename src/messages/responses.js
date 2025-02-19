816f209b44ac0eb3c9be5f2817fa98a5284b7f00
bds-yuridelima <yuridelima@vending.ie>
2025-02-19 10:24:05 +0000

    Create responses.js

---
+++ src/messages/responses.js
@@ -0,0 +1,12 @@
+module.exports = {
+    welcome: "Hello! Welcome to BDS Vending customer service. We hope we can assist you!",
+    yourName: "Can you please tell us your name?",
+    invalidName: "Hmm, it looks like you didn’t enter your name correctly! Only letters are allowed, and your name must have at least two characters. Let's try again!",
+    yourEmail: "Can you please provide your email? (If you do not wish to share, simply type '-')",
+    invalidEmail: "It looks like there is an error here. To provide your email, please make sure it follows this format: 'example@mail.xx'. If you do not wish to share your email, simply type '-' (dash).",
+    grpd: "To process your request, we will use your phone number and/or email to contact you. Your data will not be stored in any database and will not be used for any other purpose beyond this specific request. Do you agree so we can proceed with your service?",
+    vmNumber: "Great! Please provide the machine number. (The number is written on the QR Code that you just scanned.)",
+    invalidMachineNumber: "This machine number does not match our records. Please make sure you enter the correct number found on the QR Code. Let's try again!",
+    machineConfirmed: "Machine confirmed! Please choose an option from the main menu.",
+    exceededMsg: "We are sorry, but you have exceeded the number of attempts. If you want to restart this service, simply send a new message below this one or scan our QR Code again on the vending machine. If you feel more comfortable, you can call us at 1800 623 000, send an email to service@vending.ie, or find more information on our website vending.ie. Cheers!"
+};
