0f38c7b9bf15d00ada2e59e2055278871169fcaf
bds-yuridelima <yuridelima@vending.ie>
2025-02-19 15:31:46 +0000

    Delete src directory

--- src/config/settings.js
+++
@@ -1,7 +0,0 @@
-// src/config/settings.js
-const path = require('path');
-
-module.exports = {
-  sessionFilePath: path.join(__dirname, '../../session.json'),
-  limitAttempts: 5,
-};
--- src/services/session.js
+++
@@ -1,7 +0,0 @@
-const sessions = {};
-
-module.exports = {
-    getSession: (user) => sessions[user] || null,
-    resetSession: (user) => sessions[user] = { step: 'welcome', attempts: 0 },
-    updateSession: (user, step, data = {}) => sessions[user] = { ...sessions[user], step, ...data, attempts: 0 }
-};
